18abd2b08647b7550f3bd55d073524c72048e43e
melissajuarezc <mj2945@columbia.edu>
2023-04-18 15:56:58 -0400

    create custom functions for EDA

--- code/functions.R
+++ code/functions.R
@@ -3,3 +3,26 @@
 # Melissa Juarez
 
 # Use this file for storing functions to be used in the script files
+
+one_var_tabyl <- function(df, var) {
+  var = enquo(var)
+  
+  table <- df %>% janitor::tabyl(!!var) %>%
+    arrange(desc(percent)) %>% 
+    mutate(count = scales::comma(n, accuracy = 1),
+           percent = scales::percent(percent, accuracy = 0.01)) %>%
+    select(!!var, count, percent)
+  
+  return(table)
+}
+
+k <- function(table) {
+  
+  table <- table %>%
+    knitr::kable() %>%
+    kableExtra::kable_styling() %>%
+    kableExtra::scroll_box(height = "400px")
+  
+  return(table)
+  
+}
